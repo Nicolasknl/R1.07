33b33a15837c641d908f751190814cf6ae29cde0
Nicolasknl <nicolas.kunegel@uha.fr>
2025-11-03 11:35:16 +0100

    Add files via upload

---
+++ Exo 3 python .py
@@ -0,0 +1,11 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> j = int (input("Jour: "))
+Jour: 3
+>>> h = int (input("Heure: "))
+Heure: 9
+>>> m = int(input("Minute: "))
+Minute: 49
+>>> total = (j*1440+h*60+m)
+>>> total
+4909
